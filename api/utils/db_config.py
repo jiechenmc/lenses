332b010d118edf8d5755dbbaed72fe5473a7511d
Kelly Chen <kelly.chen.6@stonybrook.edu>
2025-05-19 17:18:13 -0400

    stage

--- api/utils/db_config.py
+++ api/utils/db_config.py
@@ -1,14 +1,14 @@
 from sqlmodel import SQLModel, create_engine, Session
 from ..models.db_models import * 
 
-DATABASE_URL = "postgresql://username:password@localhost:5432/mydatabase"
+DATABASE_URL = "URL HERE"
 
-# engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
-    # SQLModel.metadata.create_all(engine)
-    pass
+    SQLModel.metadata.create_all(engine)
+    # pass
 
 def get_session():
-    # return Session(engine)
-    pass
+    return Session(engine)
+    # pass
