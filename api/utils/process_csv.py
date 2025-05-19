a1908da7fb89b2f628a297503dea41559a3cdf09
Kelly Chen <kelly.chen.6@stonybrook.edu>
2025-05-19 17:15:53 -0400

    Revert "wip"
    This reverts commit 4f65f5e601d5ec45484a6f716a937f4f12154477.

--- api/utils/process_csv.py
+++ api/utils/process_csv.py
@@ -17,11 +17,9 @@ def process_crime_rates():
    with open("api/utils/data/crime_rates.csv", "r") as file: 
       reader = csv.reader(file)
       print(next(reader))
-      count = 0
       for row in reader: 
          insert_crime(row)
-         # break
-      print(count)
+         break
 
 def insert_community_area(data: list):
    # the_geom,AREA_NUMBE,COMMUNITY,AREA_NUM_1,SHAPE_AREA,SHAPE_LEN
@@ -67,4 +65,4 @@ def insert_crime(data: list):
    pass
 # process_community_area()
 # create_db_and_tables()
-process_crime_rates()
+# process_crime_rates()
