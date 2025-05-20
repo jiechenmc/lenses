2923310bff3fd47911dcbb1f0da8f6f21188f5d2
Kelly Chen <kelly.chen.6@stonybrook.edu>
2025-05-19 23:29:35 -0400

    GYAT

--- api/utils/db_config.py
+++ api/utils/db_config.py
@@ -1,7 +1,10 @@
 from sqlmodel import SQLModel, create_engine, Session
 from ..models.db_models import * 
+from dotenv import load_dotenv
+import os
+load_dotenv() 
 
-DATABASE_URL = "URL HERE"
+DATABASE_URL = os.getenv("POSTGRES")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
@@ -12,3 +15,5 @@ def create_db_and_tables():
 def get_session():
     return Session(engine)
     # pass
+def get_db_url(): 
+    return DATABASE_URL
