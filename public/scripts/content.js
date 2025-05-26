d68b74fe95b318656417282de718d77e13d86984
Jie Chen <jiechenmc@gmail.com>
2025-05-26 18:16:43 -0400

    badge display with fake data

--- public/scripts/content.js
+++ public/scripts/content.js
@@ -1,20 +1,63 @@
-const addHelloToAddresses = () => {
+const createBadge = (type, data) => {
+  const crimeThresh = { low: 0, medium: 10, high: 30 };
+  const commuteThresh = { low: 0, medium: 30, high: 60 };
+
+  const element = document.createElement("div");
+
+  switch (type) {
+    case "crime":
+      element.innerText = `👮 ${data}`;
+      if (data > crimeThresh["high"]) element.className = "badge error";
+      else if (data > crimeThresh["high"]) element.className = "badge warning";
+      else element.className = "badge success";
+      break;
+    case "commute":
+      element.innerText = `🚗 ${data}`;
+      if (data > commuteThresh["high"]) element.className = "badge error";
+      else if (data > commuteThresh["high"])
+        element.className = "badge warning";
+      else element.className = "badge success";
+      break;
+  }
+
+  return element;
+};
+
+const addBadgeToAddresses = () => {
   const addresses = document.querySelectorAll("address");
 
   for (const address of addresses) {
-    if (!address.querySelector(".badge")) {
-      const p = document.createElement("div");
-      p.innerText = "Hello World";
-      p.className = ".badge";
-      address.appendChild(p);
+    const fakeData = {
+      crime_rate: Math.floor(Math.random() * 100) + 1,
+      commute: Math.floor(Math.random() * 100) + 1,
+      schools: [],
+      convenient_stores: [],
+    };
+
+    if (!address.parentElement.parentElement.querySelector(".badge")) {
+      const crime = createBadge("crime", `${fakeData["crime_rate"]}`);
+      const commute = createBadge("commute", fakeData["commute"]);
+
+      const container = document.createElement("div");
+
+      container.appendChild(crime);
+      container.appendChild(commute);
+
+      address.parentElement.parentElement.insertAdjacentElement(
+        "beforeend",
+        container
+      );
     }
   }
 };
 
-addHelloToAddresses();
-
-const observer = new MutationObserver(() => {
-  addHelloToAddresses();
+const observer = new MutationObserver((mutations, obs) => {
+  observer.disconnect();
+  addBadgeToAddresses();
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
 });
 
 observer.observe(document.body, {
