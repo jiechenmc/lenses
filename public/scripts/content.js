f1e44538f14bd04d87e7b6291c877b8635621a5c
Jie Chen <jiechenmc@gmail.com>
2025-05-26 16:05:50 -0400

    add initial css test

--- public/scripts/content.js
+++ public/scripts/content.js
@@ -1,3 +1,23 @@
-const address = document.querySelector("address");
+const addHelloToAddresses = () => {
+  const addresses = document.querySelectorAll("address");
 
-console.log(address);
+  for (const address of addresses) {
+    if (!address.querySelector(".badge")) {
+      const p = document.createElement("div");
+      p.innerText = "Hello World";
+      p.className = ".badge";
+      address.appendChild(p);
+    }
+  }
+};
+
+addHelloToAddresses();
+
+const observer = new MutationObserver(() => {
+  addHelloToAddresses();
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
