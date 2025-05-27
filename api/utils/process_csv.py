21e9f404cbc86183abb22c64699d008513229803
Kelly Chen <kelly.chen.6@stonybrook.edu>
2025-05-27 19:38:18 -0400

    update db

--- api/utils/process_csv.py
+++ api/utils/process_csv.py
@@ -11,41 +11,43 @@ import pandas as pd
 from shapely.wkt import loads as load_wkt
 from geoalchemy2.shape import from_shape
 # List of violent crimes
-VIOLENT_CRIMES = {
-    "ARSON", 
-    "ASSAULT", 
-    "BATTERY", 
-    "CRIMINAL SEXUAL ASSAULT", 
-    "CRIMINAL TRESPASS", 
-    "HOMICIDE", 
-    "HUMAN TRAFFICKING", 
-    "INTIMIDATION", 
-    "KIDNAPPING", 
-    "MOTOR VEHICLE THEFT", 
-    "ROBBERY", 
-    "SEX OFFENSE", 
-    "STALKING", 
-    "THEFT", 
+VIOLENT_CRIMES = [
+    "ASSAULT",
+    "BATTERY",
+    "CRIMINAL SEXUAL ASSAULT",
+    "HOMICIDE",
+    "HUMAN TRAFFICKING",
+    "INTIMIDATION",
+    "KIDNAPPING",
+    "OFFENSE INVOLVING CHILDREN",
+    "ROBBERY",
+    "SEX OFFENSE",
+    "STALKING"
+]
+
+NONVIOLENT_CRIMES = [
+    "ARSON",
+    "BURGLARY",
+    "CONCEALED CARRY LICENSE VIOLATION",
+    "CRIMINAL DAMAGE",
+    "CRIMINAL TRESPASS",
+    "DECEPTIVE PRACTICE",
+    "GAMBLING",
+    "INTERFERENCE WITH PUBLIC OFFICER",
+    "LIQUOR LAW VIOLATION",
+    "MOTOR VEHICLE THEFT",
+    "NARCOTICS",
+    "NON-CRIMINAL",
+    "OBSCENITY",
+    "OTHER NARCOTIC VIOLATION",
+    "OTHER OFFENSE",
+    "PROSTITUTION",
+    "PUBLIC INDECENCY",
+    "PUBLIC PEACE VIOLATION",
+    "RITUALISM",
+    "THEFT",
     "WEAPONS VIOLATION"
-}
- 
-# List of non-violent crimes
-NON_VIOLENT_CRIMES = {
-    "CONCEALED CARRY LICENSE VIOLATION", 
-    "DECEPTIVE PRACTICE", 
-    "GAMBLING", 
-    "LIQUOR LAW VIOLATION", 
-    "NARCOTICS", 
-    "NON-CRIMINAL", 
-    "OBSCENITY", 
-    "OFFENSE INVOLVING CHILDREN", 
-    "OTHER NARCOTIC VIOLATION", 
-    "OTHER OFFENSE", 
-    "PROSTITUTION", 
-    "PUBLIC INDECENCY", 
-    "PUBLIC PEACE VIOLATION", 
-    "RITUALISM"
-}
+]
 
 def get_row_count(conn, table_name):
     with conn.cursor() as cur:
@@ -76,12 +78,15 @@ def process_community_area():
          row["the_geom"],
          row["AREA_NUMBE"],
          row["COMMUNITY"],
-         row["SHAPE_AREA"],
-         row["SHAPE_LEN"],
+         row["total_violent_crimes"],
+         row["total_nonviolent_crimes"],
          row["total_population"],
          row["total_crimes"],
+         row["violent_crime_rate"],
+         row["nonviolent_crime_rate"],
          row["crime_rate"],
          row["crime_percentile"]
+         
       ]
       insert_community_area(row)
       
@@ -105,28 +110,28 @@ def process_crime_rates():
          
          for row in reader:
             cleaned_row = [
-                parse_int(row[0]),
-                row[1] or None,
-                parse_datetime(row[2]),
-                row[3] or None,
-                row[4] or None,
-                row[5] or None,
-                row[6] or None,
-                row[7] or None,
-                parse_bool(row[8]),
-                parse_bool(row[9]),
-                parse_int(row[10]),
-                parse_int(row[11]),
-                parse_int(row[12]),
-                parse_int(row[13]),
-                row[14] or None,
-                parse_float(row[15]),
-                parse_float(row[16]),
-                parse_int(row[17]),
-                parse_datetime(row[18]),
-                parse_float(row[19]),
-                parse_float(row[20]),
-                row[21] or None
+                parse_int(row[0]), # Crime ID
+                row[1] or None, # Case Number
+                parse_datetime(row[2]), # Date
+               #  row[3] or None, # Block 
+                row[4] or None, # IUCR 
+                row[5] or None, # Primary Type
+               #  row[6] or None, # Description
+               #  row[7] or None, # Location Description
+                parse_bool(row[8]), # Arrest
+                parse_bool(row[9]), # Domestic
+               #  parse_int(row[10]), # Beat
+               #  parse_int(row[11]), # District
+               #  parse_int(row[12]), # Ward
+                parse_int(row[13]), # Community Area
+                row[14] or None, # FBI Code
+               #  parse_float(row[15]), # X-Coordinate
+               #  parse_float(row[16]), # Y-Coordinate
+                parse_int(row[17]), # Year
+               #  parse_datetime(row[18]), # Updated On
+                parse_float(row[19]), # Latitude
+                parse_float(row[20]), # Longitude 
+               #  row[21] or None # Location
             ]
             writer.writerow(cleaned_row)
             # print(cleaned_row)
@@ -138,11 +143,8 @@ def process_crime_rates():
    with open(tmp_file.name, 'r') as f: 
       cur.copy_expert(
          f"""COPY crimerates (
-    crime_id, case_number, date, block, iucr, primary_type,
-    description, location_description, arrest, domestic,
-    beat, district, ward, community_area, fbi_code,
-    x_coordinate, y_coordinate, year, updated_on,
-    latitude, longitude, location
+    crime_id, case_number, date, iucr, primary_type, arrest, 
+    domestic, community_area, fbi_code, year, latitude, longitude
 ) FROM STDIN WITH CSV HEADER;""",
          f
       )
@@ -164,6 +166,22 @@ def compute_crime_per_capita(community_areas):
    # Drop rows without a community area
    crimes = crimes[crimes['Community Area'].notna()]
    crimes['Community Area'] = crimes['Community Area'].astype(int)
+   
+   # Violent Crimes 
+   violent_counts = (
+      crimes[crimes["Primary Type"].isin(VIOLENT_CRIMES)]
+      .groupby("Community Area")
+      .size()
+      .reset_index(name="total_violent_crimes")
+   )
+
+   # Non-Violent Crimes
+   nonviolent_counts = (
+      crimes[crimes["Primary Type"].isin(NONVIOLENT_CRIMES)]
+      .groupby("Community Area")
+      .size()
+      .reset_index(name="total_nonviolent_crimes")
+   )
 
    # Group by community area
    crime_counts = (
@@ -171,12 +189,19 @@ def compute_crime_per_capita(community_areas):
       .size()
       .reset_index(name="total_crimes")
    )
-
+   # Merge crime counts with violent and nonviolent crime counts
+   all_counts = pd.merge(crime_counts, violent_counts, on="Community Area", how="left")
+   all_counts = pd.merge(all_counts, nonviolent_counts, on="Community Area", how="left")
    # Merge with community areas table with the total population information
-   crime_counts = crime_counts.rename(columns={"Community Area": "AREA_NUMBE"})
-   merged = crime_counts.merge(community_areas, on="AREA_NUMBE")
+   all_counts = all_counts.rename(columns={"Community Area": "AREA_NUMBE"})
+   merged = all_counts.merge(community_areas, on="AREA_NUMBE")
    # Calculate Crime Rate per 100000
    merged["crime_rate"] = merged["total_crimes"] / merged["total_population"] * 100000
+   # Calculate Violent Crime Rate per 100000
+   merged["violent_crime_rate"] = merged["total_violent_crimes"] / merged["total_population"] * 100000
+   # Calculate Non-violent Crime Rate per 100000
+   merged["nonviolent_crime_rate"] = merged["total_nonviolent_crimes"] / merged["total_population"] * 100000
+
    # The lower the percentile, the safer the neighborhood is compared to its peers
    merged["crime_percentile"] = merged["crime_rate"].rank(pct=True)
    print(merged.head())   
@@ -188,12 +213,14 @@ def insert_community_area(data: list):
       area_id=int(data[1]), 
       the_geom=from_shape(load_wkt(data[0]), srid=4326),
       name=data[2], 
-      # shape_area=int(data[3]),
-      # shape_len=float(data[4]),
+      total_violent_crimes=int(data[3]),
+      total_nonviolent_crimes=float(data[4]),
       total_population=data[5],
       total_crimes=data[6],
       crime_rate=data[7],
-      crime_percentile=data[8]
+      violent_crime_rate=data[8],
+      nonviolent_crime_rate=data[9],
+      crime_percentile=data[10]
       )
    with get_session() as session:
       session.add(community_area)
@@ -235,5 +262,5 @@ def insert_crime(data: list):
        print(f"Inserted Crime ID: {crime.id}")
    pass
 create_db_and_tables()
-# process_community_area()
+process_community_area()
 process_crime_rates()
