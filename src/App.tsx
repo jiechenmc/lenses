833a917ac184f66194e117974eb34a6a7e072867
Jie Chen <jiechenmc@gmail.com>
2025-05-26 22:36:18 -0400

    add map

--- src/App.tsx
+++ src/App.tsx
@@ -1,134 +1,45 @@
-// import { useState } from "react";
+import { useState } from "react";
+import PriceCard from "./components/PriceCard";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import type { LatLngExpression } from "leaflet";
+type City = "Chicago" | "New York City";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [city, setCity] = useState<City>("Chicago");
 
-  // const crimeScore = 0;
-  // const commuteScore = 0;
-  // const schools = [];
-  // const convenient_stores = [];
+  const cityCoord: Record<City, LatLngExpression> = {
+    Chicago: [41.8781, -87.6298],
+    "New York City": [40.7128, -74.006],
+  };
 
   return (
-    <>
-      <div className="card w-96 bg-base-100 shadow-sm">
-        <div className="card-body">
-          <span className="badge badge-xs badge-warning">Most Popular</span>
-          <div className="flex justify-between">
-            <h2 className="text-3xl font-bold">Premium</h2>
-            <span className="text-xl">$29/mo</span>
-          </div>
-          <ul className="mt-6 flex flex-col gap-2 text-xs">
-            <li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 me-2 inline-block text-success"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-              <span>High-resolution image generation</span>
-            </li>
-            <li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 me-2 inline-block text-success"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-              <span>Customizable style templates</span>
-            </li>
-            <li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 me-2 inline-block text-success"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-              <span>Batch processing capabilities</span>
-            </li>
-            <li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 me-2 inline-block text-success"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-              <span>AI-driven image enhancements</span>
-            </li>
-            <li className="opacity-50">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 me-2 inline-block text-base-content/50"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-              <span className="line-through">Seamless cloud integration</span>
-            </li>
-            <li className="opacity-50">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 me-2 inline-block text-base-content/50"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-              <span className="line-through">
-                Real-time collaboration tools
-              </span>
-            </li>
-          </ul>
-          <div className="mt-6">
-            <button className="btn btn-primary btn-block">Subscribe</button>
-          </div>
-        </div>
+    <div className="p-4">
+      <div className="w-full text-center">
+        <select
+          defaultValue="Chicago"
+          className="select select-info"
+          onChange={(e) => setCity(e.target.value as City)}
+        >
+          <option disabled={true}>Pick a City</option>
+          <option>Chicago</option>
+          <option>New York City</option>
+        </select>
       </div>
-    </>
+      <MapContainer
+        className="mx-auto mt-4"
+        key={city}
+        center={cityCoord[city]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+      <PriceCard />
+    </div>
   );
 }
 
