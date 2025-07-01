1ac0f9f4b86330869ea5900dc05f04b6fc3ff6e7
Kelly Chen <kelly.chen.6@stonybrook.edu>
2025-06-30 22:13:39 -0400

    documentation up

--- api/main.py
+++ api/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI, HTTPException
 from models.insights import Insights
 
-app = FastAPI()
+description = """
+Lenses API makes real estate search easier! 
+"""
+app = FastAPI(    
+    title="Lenses API",
+    description=description,
+    summary="Lenses: Real Estate's Best Friend",
+    version="0.0.1",
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    })
 
 
 @app.get("/")
