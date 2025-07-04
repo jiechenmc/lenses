2dfb1957cf6736b9998d36f84b7fcb31e23b1c70
Jie Chen <jiechenmc@gmail.com>
2025-07-03 21:46:41 -0500

    add code to modify the DOM

--- public/scripts/content.js
+++ public/scripts/content.js
@@ -1,3 +1,35 @@
-const address = document.querySelector("address");
+const address_node = document.querySelector("address");
 
-console.log(address);
+const origin = encodeURI(address_node.textContent);
+const dest = encodeURI("600 W Chicago Ave, Chicago");
+const api = `http://34.229.111.5:8000/api/get_commute?origin=${origin}&destination=${dest}&mode=transit`;
+
+fetch(api).then((r) =>
+  r.json().then((d) => {
+    console.log(d);
+  })
+);
+
+function injectHi(address_node) {
+  const id = "chrome-ext-hi";
+  if (!document.getElementById(id)) {
+    const span = document.createElement("div");
+    span.textContent = "6 Minutes";
+    span.id = id;
+    address_node.parentNode.insertAdjacentElement("afterend", span);
+  }
+}
+
+// Initial injection
+injectHi(address_node);
+
+// Watch for React wiping it out
+const observer = new MutationObserver(() => {
+  if (!document.getElementById("chrome-ext-hi")) {
+    injectHi(address_node);
+  }
+});
+
+observer.observe(address_node.parentNode, {
+  childList: true,
+});
