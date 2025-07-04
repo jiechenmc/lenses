6e6a863a80d487dfabefd5ae56112ec34d74099f
Jie Chen <jiechenmc@gmail.com>
2025-07-03 22:21:28 -0500

    add cors

--- api/main.py
+++ api/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI, HTTPException
 from models.insights import Insights
+from fastapi.middleware.cors import CORSMiddleware
 
 description = """
 Lenses API makes real estate search easier! 
 """
-app = FastAPI(    
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app = FastAPI(
     title="Lenses API",
     description=description,
     summary="Lenses: Real Estate's Best Friend",
@@ -12,52 +20,73 @@ app = FastAPI(
     license_info={
         "name": "Apache 2.0",
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
-    })
+    },
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
-async def root(): 
+async def root():
     return {"message": "Hello World"}
+
 
 @app.get("/api/get_insight")
 async def get_insight():
-    # Takes in an address 
+    # Takes in an address
     return {"message": "Hello World"}
+
 
 # Given an address, compute the safety score
 @app.get("/api/get_safety_score/")
 async def get_safety_score(address: str):
     community, community_id = Insights.get_community_area(address)
     if not community_id:
-        raise HTTPException(status_code=404, detail="Invalid address or community area not found.")
+        raise HTTPException(
+            status_code=404, detail="Invalid address or community area not found."
+        )
     safety_score = Insights.get_crime_percentile(community_id)
     return {
         "address": address,
         "community_area": community,
-        "safety_score": safety_score
+        "safety_score": safety_score,
     }
 
+
 @app.get("/api/get_commute/")
-async def get_commute(origin:str, destination:str, mode:str="drive"):
+async def get_commute(origin: str, destination: str, mode: str = "drive"):
     print(f"Origin: {origin}, Destination: {destination}")
     modes = ["drive", "walk", "bicycle", "transit"]
     if mode not in modes:
-        raise HTTPException(status_code=400, detail="Invalid mode of transportation. Choose from: drive, walk, bicycle, transit.")
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid mode of transportation. Choose from: drive, walk, bicycle, transit.",
+        )
     # Calculate commute time and distance
     commute_info = Insights.get_commute(origin, destination, mode)
     if not commute_info:
-        raise HTTPException(status_code=500, detail="Failed to compute commute information.")
+        raise HTTPException(
+            status_code=500, detail="Failed to compute commute information."
+        )
     return {
         "origin": origin,
         "destination": destination,
         "mode": mode,
         "distance_meters": commute_info["distanceMeters"],
-        "duration": commute_info["duration"]
+        "duration": commute_info["duration"],
     }
+
 
 @app.get("/api/get_schools")
 async def get_schools():
     return {"message": "Schools Route"}
+
 
 @app.get("/api/get_convenience_stores")
 async def get_convenience_stores():
--- public/scripts/content.js
+++ public/scripts/content.js
@@ -2,31 +2,31 @@ const address_node = document.querySelector("address");
 
 const origin = encodeURI(address_node.textContent);
 const dest = encodeURI("600 W Chicago Ave, Chicago");
-const api = `http://34.229.111.5:8000/api/get_commute?origin=${origin}&destination=${dest}&mode=transit`;
+const api = `https://lenses.jiechen.dev/api/get_commute?origin=${origin}&destination=${dest}&mode=transit`;
+
+let latestCommuteTime = null;
 
 fetch(api).then((r) =>
   r.json().then((d) => {
-    console.log(d);
+    latestCommuteTime = d;
+    injectHi(address_node, latestCommuteTime);
   })
 );
 
-function injectHi(address_node) {
+function injectHi(address_node, time) {
   const id = "chrome-ext-hi";
   if (!document.getElementById(id)) {
     const span = document.createElement("div");
-    span.textContent = "6 Minutes";
+    span.textContent = `${time} Minutes`;
     span.id = id;
     address_node.parentNode.insertAdjacentElement("afterend", span);
   }
 }
 
-// Initial injection
-injectHi(address_node);
-
 // Watch for React wiping it out
 const observer = new MutationObserver(() => {
-  if (!document.getElementById("chrome-ext-hi")) {
-    injectHi(address_node);
+  if (!document.getElementById("chrome-ext-hi") && latestCommuteTime !== null) {
+    injectHi(address_node, latestCommuteTime);
   }
 });
 
