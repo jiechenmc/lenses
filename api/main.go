063a21a9f8287d61369d265ea68216c7b0cb9166
Jie Chen <jiechenmc@gmail.com>
2026-03-21 15:18:18 -0500

    init

---
+++ api/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/duckdb/duckdb-go/v2"
+)
+
+func main() {
+	db, err := sql.Open("duckdb", "data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS crime AS
+		SELECT * FROM read_csv('data/data.csv', header = true)
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var (
+		id   int
+		name string
+	)
+
+	// Query all rows
+	rows, err := db.Query(`SELECT id, date FROM crime`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("id: %d, date: %s\n", id, name)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
